f3d5c695fb3e7e7d9e473ff33706b8f00e879515
Gabriela Lichy <gaabi.lichy@gmail.com>
2021-07-27 21:01:15 -0300

    segundo dia bootcamp

--- findfood/src/app/app-routing.module.ts
+++ findfood/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: RestaurantesComponent,
+    pathMatch: 'full'
+  },{
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- findfood/src/app/app.module.ts
+++ findfood/src/app/app.module.ts
@@ -5,21 +5,51 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//material
-import {MatCardModule} from '@angular/material/card';
-
+// material
+import { MatCardModule } from '@angular/material/card';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatSelectModule} from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FiltroRestaurantePipe } from './shared/filtro-restaurante.pipe';
+import { RestauranteComponent } from './restaurante/restaurante.component';
+import { NovoRestauranteComponent } from './novo-restaurante/novo-restaurante.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantesComponent,
+    FiltroRestaurantePipe,
+    RestauranteComponent,
+    NovoRestauranteComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
+    //Forms
+    ReactiveFormsModule,
+    FormsModule,
+
     // material
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
